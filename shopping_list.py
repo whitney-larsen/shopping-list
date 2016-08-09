57e4a64433762210e223299037d3bd3a25a7c574
Your Name <you@example.com>
2016-08-08 21:07:40 -0700

    second commit

--- shopping_list.py
+++ shopping_list.py
@@ -1,20 +1,26 @@
 shopping_list = ["bananas","milk","eggs","sugar","butter"]
 
-def add_shopping_list(shopping_list):
+def current_shopping_list():
+	for item in shopping_list:
+		print item
+
+def add_shopping_list():
 	new_item=raw_input("What do you want to add?").lower()
 
+	global shopping_list
 	if shopping_list.count(new_item) == 0:
 		shopping_list = shopping_list + [new_item]
 	else:
 		print "That item is already on your list."
 
-	alphabetical_order(shopping_list)
+	alphabetical_order()
 
-def alphabetical_order(shopping_list):
+def alphabetical_order():
+	global shopping_list
 	shopping_list.sort()
-	print shopping_list
+	current_shopping_list()
 
-def remove_from_list(shopping_list): 
+def remove_from_list(): 
 	remove_item = raw_input("what do you want to remove?").lower() 
 
 	if shopping_list.count(remove_item) == 0: 
@@ -22,13 +28,24 @@ def remove_from_list(shopping_list):
 	else: 
 		shopping_list.remove(remove_item)   
 		
-	alphabetical_order(shopping_list)
+	alphabetical_order()
+
+def menu():
+	choice = int(raw_input("0 - Exit the program, 1 - Show current list, 2 - Add an item to your list, 3 - Remove an item from your list"))
+	return choice
 
 def main():
-	add_shopping_list(shopping_list)
-	add_shopping_list(shopping_list)	
-	remove_from_list(shopping_list)
-	remove_from_list(shopping_list)
+	while(True):
+		choice=menu()
+		if choice==1:
+			current_shopping_list()
+		elif choice==2:
+			add_shopping_list()
+		elif choice==3:
+			remove_from_list()
+		elif choice==0:
+			break
+
 
 if __name__ == '__main__':
 	main()
